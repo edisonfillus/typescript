a9bf377bfe53c3ee0bf36278d6f0a0fe1a41f133
Edison Klafke Fillus <edison.fillus@gmail.com>
2019-09-06 17:51:55 -0300

    first step

---
+++ alurabank/app/js/controllers/NegociacaoController.js
@@ -0,0 +1,7 @@
+class NegociacaoController {
+    constructor() {
+        this._inputData = document.querySelector('#data');
+        this._inputQuantidade = document.querySelector('#quantidade');
+        this._inputValor = document.querySelector('#valor');
+    }
+}
---
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -0,0 +1,13 @@
+class NegociacaoController {
+
+    private _inputData: HTMLInputElement;
+    private _inputQuantidade: HTMLInputElement;
+    private _inputValor: HTMLInputElement;
+
+    constructor() {
+
+        this._inputData = <HTMLInputElement>document.querySelector('#data');
+        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
+        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+    }
+}
